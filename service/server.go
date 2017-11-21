98b226d68157f1f8b04cba8b1cb4c2bb226f3cb8
CoolHorse <18142823152@163.com>
2017-11-21 20:34:04 +0800

    finish

--- service/server.go
+++ service/server.go
@@ -1,10 +1,8 @@
 package service
 
 import (
-	"fmt"
 	"net/http"
 	"os"
-	"strings"
 
 	"github.com/codegangsta/negroni"
 	"github.com/gorilla/mux"
@@ -42,33 +40,6 @@ func initRoutes(mx *mux.Router, formatter *render.Render) {
 	mx.HandleFunc("/login", loginHandler(formatter)).Methods("POST", "GET")
 	mx.HandleFunc("/", loginHandler(formatter)).Methods("GET")
 	mx.PathPrefix("/").Handler(http.FileServer(http.Dir(webRoot + "/assets/")))
-	mx.Walk(func(route *mux.Route, router *mux.Router, ancestors []*mux.Route) error {
-		t, err := route.GetPathTemplate()
-		if err != nil {
-			return err
-		}
-		qt, err := route.GetQueriesTemplates()
-		if err != nil {
-			return err
-		}
-		// p will contain regular expression is compatible with regular expression in Perl, Python, and other languages.
-		// for instance the regular expression for path '/articles/{id}' will be '^/articles/(?P<v0>[^/]+)$'
-		p, err := route.GetPathRegexp()
-		if err != nil {
-			return err
-		}
-		// qr will contain a list of regular expressions with the same semantics as GetPathRegexp,
-		// just applied to the Queries pairs instead, e.g., 'Queries("surname", "{surname}") will return
-		// {"^surname=(?P<v0>.*)$}. Where each combined query pair will have an entry in the list.
-		qr, err := route.GetQueriesRegexp()
-		if err != nil {
-			return err
-		}
-		m, err := route.GetMethods()
-		if err != nil {
-			return err
-		}
-		fmt.Println(strings.Join(m, ","), strings.Join(qt, ","), strings.Join(qr, ","), t, p)
-		return nil
-	})
+	mx.PathPrefix("/static").Handler(http.StripPrefix("/static/", http.FileServer(http.Dir(webRoot+"/assets/"))))
+
 }
